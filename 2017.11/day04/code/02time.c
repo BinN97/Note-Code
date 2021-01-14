608c9bd64b4d8ad2356a3c7fc507eb9f7f09f540
BinN97 <nbin120692@gmail.com>
2021-01-14 20:09:43 +0800

    删除无用部分

--- 2017.11/day04/code/02time.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-int main(){
-    int s=time(0);
-    printf("%d\n",s);
-    return 0;
-}
--- 2017.11/day04/code/04year.c
+++
@@ -1,9 +0,0 @@
-//求平闰年
-#include<stdio.h>
-int main(){
-    int year=0;
-    printf("请输入:");
-    scanf("%d",&year);
-    (year%4==0&&year%100!=0)||(year%400==0)?printf("闰年\n"):printf("平年\n");
-    return 0;
-    }
